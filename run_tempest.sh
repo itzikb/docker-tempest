e6616966fb97280bb3d7a21fea23d45b06c19c2a
Itzik <itzikb@users.noreply.github.com>
2016-11-24 14:22:02 +0200

    Update run_tempest.sh

--- run_tempest.sh
+++ run_tempest.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+DS_REPO_TEMPEST=/home/fedora/redhat-tempest/tempest
+US_REPO_TEMPEST=/home/fedora/tempest-upstream/tempest
+US_REPO_NEUTRON=/home/fedora/tempest-upstream/neutron
+for i in ${DS_REPO_TEMPEST} ${US_REPO_TEMPEST} ${US_REPO_NEUTRON};do
+    git checkout master && git pull
+done
 cd /home/fedora/redhat-tempest/tempest
 source ../bin/activate
 python tools/config_tempest.py identity.uri $OS_AUTH_URL identity.admin_password $OS_PASSWORD

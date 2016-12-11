f2c672a0d39fb53ed49cb85f18b47adfce46e160
Itzik <itzikb@users.noreply.github.com>
2016-12-11 14:10:33 +0200

    Updated run_tempest.sh to include neutron-lbaas

--- run_tempest.sh
+++ run_tempest.sh
@@ -2,9 +2,12 @@
 DS_REPO_TEMPEST=/home/fedora/redhat-tempest/tempest
 US_REPO_TEMPEST=/home/fedora/tempest-upstream/tempest
 US_REPO_NEUTRON=/home/fedora/tempest-upstream/neutron
-for i in ${DS_REPO_TEMPEST} ${US_REPO_TEMPEST} ${US_REPO_NEUTRON};do
+US_REPO_LBAAS=/home/fedora/tempest-upstream/neutron-lbaas
+
+for i in ${DS_REPO_TEMPEST} ${US_REPO_TEMPEST} ${US_REPO_NEUTRON} ${US_REPO_LBAAS};do
     git checkout master && git pull
 done
+
 cd /home/fedora/redhat-tempest/tempest
 source ../bin/activate
 python tools/config_tempest.py identity.uri $OS_AUTH_URL identity.admin_password $OS_PASSWORD

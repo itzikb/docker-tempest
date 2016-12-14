c21110cb8e5947a2067fef997ecd4ebe4bb6bb04
Itzik <itzikb@users.noreply.github.com>
2016-12-14 02:51:51 +0200

    Updated run_tempest.sh to centos

--- run_tempest.sh
+++ run_tempest.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-DS_REPO_TEMPEST=/home/fedora/redhat-tempest/tempest
-US_REPO_TEMPEST=/home/fedora/tempest-upstream/tempest
-US_REPO_NEUTRON=/home/fedora/tempest-upstream/neutron
-US_REPO_LBAAS=/home/fedora/tempest-upstream/neutron-lbaas
+DS_REPO_TEMPEST=/home/centos/redhat-tempest/tempest
+US_REPO_TEMPEST=/home/centos/tempest-upstream/tempest
+US_REPO_NEUTRON=/home/centos/tempest-upstream/neutron
+US_REPO_LBAAS=/home/centos/tempest-upstream/neutron-lbaas
 
 for i in ${DS_REPO_TEMPEST} ${US_REPO_TEMPEST} ${US_REPO_NEUTRON} ${US_REPO_LBAAS};do
     git checkout master && git pull
 done
 
-cd /home/fedora/redhat-tempest/tempest
+cd /home/centos/redhat-tempest/tempest
 source ../bin/activate
 python tools/config_tempest.py identity.uri $OS_AUTH_URL identity.admin_password $OS_PASSWORD --create
 sudo cp etc/tempest.conf /etc/tempest
-cd /home/fedora/tempest-upstream/tempest
+cd /home/centos/tempest-upstream/tempest
 source ../bin/activate

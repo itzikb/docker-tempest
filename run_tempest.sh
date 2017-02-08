1b303f0546c8dcc549ee9554ba82878e62466688
Itzik <itzikb@users.noreply.github.com>
2017-02-08 16:27:02 +0200

    Update run_tempest.sh

--- run_tempest.sh
+++ run_tempest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DS_REPO_TEMPEST=/home/centos/redhat-tempest/tempest
+DS_REPO_TEMPEST=/home/centos/redhat-tempest/python-tempestconf
 US_REPO_TEMPEST=/home/centos/tempest-upstream/tempest
 US_REPO_NEUTRON=/home/centos/tempest-upstream/neutron
 US_REPO_LBAAS=/home/centos/tempest-upstream/neutron-lbaas
@@ -8,9 +8,9 @@ for i in ${DS_REPO_TEMPEST} ${US_REPO_TEMPEST} ${US_REPO_NEUTRON} ${US_REPO_LBAA
     git checkout master && git pull
 done
 
-cd /home/centos/redhat-tempest/tempest
+cd /home/centos/redhat-tempest/python-tempestconf
 source ../bin/activate
-python tools/config_tempest.py identity.uri $OS_AUTH_URL identity.admin_password $OS_PASSWORD --create
+python config_tempest/config_tempest.py identity.uri $OS_AUTH_URL identity.admin_password $OS_PASSWORD --create
 sudo cp etc/tempest.conf /etc/tempest
 cd /home/centos/tempest-upstream/tempest
 source ../bin/activate

014e0c4d63cc0bf8b2dc88c6ec4207c050414ae6
Itzik <itzikb@users.noreply.github.com>
2018-02-13 11:56:03 +0200

    Update run_tempest.sh

--- scripts/run_tempest.sh
+++ scripts/run_tempest.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 sudo chmod +rx /env
 source /env/overcloudrc
-DS_REPO_TEMPEST=/home/centos/tempset-upstream/python-tempestconf
+DS_REPO_TEMPEST=/home/centos/tempest-upstream/python-tempestconf
 US_REPO_TEMPEST=/home/centos/tempest-upstream/tempest
 US_REPO_NEUTRON=/home/centos/tempest-upstream/neutron-tempest-plugin
 US_REPO_LBAAS=/home/centos/tempest-upstream/neutron-lbaas
 
 for i in ${US_REPO_TEMPEST} ${US_REPO_NEUTRON} ${US_REPO_LBAAS};do
-    pushd && git checkout master && git pull && popd
+    pushd $i && git checkout master && git pull && popd
 done
 
-pushd ${DS_REPO_TEMPEST}
-git checkout 1.1.3
-popd
-
-cd /home/centos/tempest-upstream/python-tempestconf
 source ../bin/activate
+
+pushd ${DS_REPO_TEMPEST}
+git checkout 1.1.3 -b 1.1.3
+pip intall -e .
 python config_tempest/config_tempest.py identity.uri $OS_AUTH_URL identity.admin_password $OS_PASSWORD DEFAULT.use_stderr true identity.region regionOne validation.run_validation True --create
 sudo cp etc/tempest.conf /etc/tempest
 if [ "${OTHER_CONFIG}" ]; then
   bash ${OTHER_CONFIG}
 fi
 cd /home/centos/tempest-upstream/tempest
-source ../bin/activate

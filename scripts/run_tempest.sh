f6dfdf9c803edd3df2c10f0b9a292768431fc111
Itzik <itzikb@users.noreply.github.com>
2018-04-15 12:29:47 +0300

    Updating run_tempest.sh
    Updating to adapt to changes in python-tempestconf

--- scripts/run_tempest.sh
+++ scripts/run_tempest.sh
@@ -12,10 +12,11 @@ done
 
 cd /home/centos/tempest-upstream/python-tempestconf
 source ../bin/activate
-python config_tempest/config_tempest.py identity.uri $OS_AUTH_URL identity.admin_password $OS_PASSWORD DEFAULT.use_stderr true identity.region regionOne validation.run_validation True --create
+python config_tempest/tempest_conf.py identity.uri $OS_AUTH_URL identity.admin_password $OS_PASSWORD DEFAULT.use_stderr true identity.region regionOne validation.run_validation True --create
 sudo cp etc/tempest.conf /etc/tempest
 if [ "${OTHER_CONFIG}" ]; then
   bash ${OTHER_CONFIG}
 fi
 cd /home/centos/tempest-upstream/tempest
 source ../bin/activate
+stestr init

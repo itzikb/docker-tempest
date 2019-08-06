26a698be543bb4a649bad85bfd42b346cda2811f
Itzik <itzikb@users.noreply.github.com>
2019-08-06 12:14:07 +0300

    Update run_tempest.sh

--- scripts/run_tempest.sh
+++ scripts/run_tempest.sh
@@ -4,7 +4,7 @@ sudo chmod +rx /home/centos/overcloudrc
 sudo chown centos /home/centos/overcloudrc
 sudo sed -i 's/v2.*/v3/g' /home/centos/overcloudrc
 source /home/centos/overcloudrc
-DS_REPO_TEMPEST=/home/centos/tempset-upstream/python-tempestconf
+#DS_REPO_TEMPEST=/home/centos/tempset-upstream/python-tempestconf
 US_REPO_TEMPEST=/home/centos/tempest-upstream/tempest
 
 for i in ${DS_REPO_TEMPEST} ${US_REPO_TEMPEST};do
@@ -13,10 +13,11 @@ done
 
 cd /home/centos/tempest-upstream/python-tempestconf
 source ../bin/activate
-# Temporary because failing with the last commit of python-tempestconf
-git checkout c1951c379e226f1ee4ea537408c166379d7808ad
+pip install -U python-tempestconf
 
-python config_tempest/main.py identity.uri $OS_AUTH_URL identity.admin_password $OS_PASSWORD DEFAULT.use_stderr true identity.region regionOne validation.run_validation True load_balancer.enable_security_groups True load_balancer.member_role _member_ load_balancer.admin_role admin load_balancer.RBAC_test_type owner_or_admin --create
+#python config_tempest/main.py identity.uri $OS_AUTH_URL identity.admin_password $OS_PASSWORD DEFAULT.use_stderr true identity.region regionOne validation.run_validation True load_balancer.enable_security_groups True load_balancer.member_role _member_ load_balancer.admin_role admin load_balancer.RBAC_test_type owner_or_admin --create
+discover-tempest-config  --os-region-name regionOne DEFAULT.use_stderr true validation.run_validation True load_balancer.enable_security_groups True load_balancer.member_role _member_ load_balancer.admin_role admin load_balancer.RBAC_test_type owner_or_admin --create
+#sudo discover-tempest-config --out /etc/tempest/tempest.conf
 sudo cp etc/tempest.conf /etc/tempest
 if [ "${OTHER_CONFIG}" ]; then
   bash ${OTHER_CONFIG}
